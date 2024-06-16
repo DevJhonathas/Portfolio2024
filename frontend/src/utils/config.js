7801c476ee7e9b3e6c45fb50ca4ab589f8316ff0
Jhonathas <jhonathassouza8@gmail.com>
2024-06-16 14:56:12 -0300

    Finalizando responsividade na página home, página projetos

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,6 +1,6 @@
-export const api = "http://localhost:5000";
-export const uploads = "http://localhost:5000/projects";
-export const photos_uploads = "http://localhost:5000/uploads/"
+export const api = "portfolio2024-zbyj.onrender.com";
+export const uploads = "portfolio2024-zbyj.onrender.com/projects";
+export const photos_uploads = "portfolio2024-zbyj.onrender.com/uploads/"
 
 export const requestConfig = (method, data, token=null, image=null) => {
     let config;
