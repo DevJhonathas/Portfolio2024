8c67153171190ca5752d1a695df9da090c8f4bd9
Jhonathas <jhonathassouza8@gmail.com>
2024-06-16 15:27:35 -0300

    configurando Api e aplicação em vercel e onRender

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 //Solve CORS
-app.use(cors({credentials: true, origin:`${process.env.PORT_FRONT}`})); 
+app.use(cors({credentials: true, origin:"https://portfolio2024-e4t3.vercel.app"})); 
 
 app.use(express.json());  
 
